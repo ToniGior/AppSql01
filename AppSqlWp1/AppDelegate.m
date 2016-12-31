c2272c55a2be7281350013b30b43440f98398b76
antonino giorgetti <antoninogiorgetti@macbook-pro-di-antonino.local>
2016-12-31 18:29:19 +0100

    Prova commit1

--- AppSqlWp1/AppDelegate.m
+++ AppSqlWp1/AppDelegate.m
@@ -18,13 +18,14 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
      NSLog(@"AppDelegate - didFinishLaunching   \n");
+    
     return YES;
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-    NSLog(@"AppDelegate - applicationWillResignActive     \n");
+    NSLog(@"AppDelegate XXXX- applicationWillResignActive     \n");
     
 }
 
